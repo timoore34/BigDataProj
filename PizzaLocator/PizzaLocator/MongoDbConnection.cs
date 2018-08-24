9f536e7c5bdeed6914c9e49203991a46e9bdf3f2
TinaMoore <TMoore@student.neumont.edu>
2018-08-23 18:11:03 -0600

    Finished GUI

---
+++ PizzaLocator/PizzaLocator/MongoDbConnection.cs
@@ -0,0 +1,61 @@
+﻿using Windows.System;
+using Windows.System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaLocator
+{
+    class MongoDbConnection
+    {
+
+        #region Main Method 
+        static void Main(string[] args)
+        {
+            //This is how to connect to your mongo instance.
+            //You will probably need to change the ipAddress part of this string to match the
+            //ipAddress of your linux system. you can keep the port the same.
+            //If you are having trouble connecting then run the command
+            //"mongod --host (linux ipAddress)" and make sure you have an instance of mongo running.
+            MongoClient client = new MongoClient("mongodb://192.168.152.129:27017");
+
+            //Creates the database if it doesnt already exist.
+            //Gets the db.
+            IMongoDatabase db = client.GetDatabase("PLDatabase");
+            try 
+            {
+                //Drops these collections if they exist.
+                //You can comment out this try catch if you want.
+                //I just didn't want so just keep adding to my db every time
+                //while I was testing.
+                db.DropCollection("PizzaStores");
+                db.DropCollection("Reviews");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+ 
+        }
+        #endregion 
+
+        #region StaticVariables
+        private static List<Reviews> reviewList = new List<Reviews>();
+        private static List<PizzaStores> listOfStores = new List<PizzaStores>(); 
+        #endregion
+
+        #region Importing Data
+        /// <summary>
+        /// Populates the static variables region with the data from yelp
+        /// </summary>
+        /// Might be easier to import the data manual before connecting, will come back to this
+        private static void DataImport()
+        { 
+        }
+        #endregion
+
+        #region Query
+        #endregion 
+    }
+}
---
+++ PizzaLocator/PizzaLocator/PizzaStores.cs
@@ -0,0 +1,52 @@
+﻿using Windows.System; 
+namespace PizzaLocator
+{
+    class PizzaStores
+        {
+            //This is how you tell mongo that this property is the
+            //c# equivolent to _id in mongo
+            [BsonId]
+            public ObjectId ID { get; set; }
+
+            //The mongo object of the same key name
+            //will be stored here. Stores the name of the store 
+            [BsonElement("name")]
+            public string Name { get; set; }
+
+            //The mongo object of the same key name
+            //will be stored here. Stores the list
+            //of categories the store is in. 
+            [BsonElement("categories")]
+            public ArrayList<string> Categories { get; set; }
+
+            //The mongo object of the same key name
+            //will be stored here. Stores up to the 
+            //first 5 reviews in the system for this restruants 
+            [BsonElement("top5Reviews")]
+            public ArrayList<Reviews> Reviews { get; set; }
+
+            //The mongo object of the same key name
+            //will be stored here. Stores the store's city
+            [BsonElement("city")]
+            public string City { get; set;  }
+
+            //The mongo objet of the same key name
+            //will be stored here. Stores the state 
+            [BsonElement("state")]
+            public string State { get; set;  }
+
+
+            /// <summary>
+            /// ToString Override
+            /// </summary>
+            /// <returns>How I would like the string to be displayed</returns>
+            public override string ToString()
+            {
+                string reviewsString = "";
+                Reviews.ForEach(x => reviewsString += $"\n\t{x.ToString()}");
+                string catString = "";
+                Categories.ForEach(x => catString += $"\n\t{x}");
+                return $"StoreID : {ID}, Name: {Name}, \nReviews: {reviewsString} \nCategories: {catString}";
+            } 
+        }
+}
